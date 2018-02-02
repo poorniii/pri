fafd18940ac46d54776d209976d693c40d206b35
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-02 15:55:14 +0530

    INTEGER.C

---
+++ INTEGER.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,r;
+i=0;
+r=arr-size-1;
+while(1<r)
+if(A[1]+A[r]==sum)
+return 1;
+else if(A[1]+A[r]sum)
+i++;
+else
+r--;
+}
+return 0;
+}
